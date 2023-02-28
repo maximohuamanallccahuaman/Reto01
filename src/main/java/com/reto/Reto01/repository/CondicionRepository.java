e7ff0e5a5f34d96665602493be7df61c4e2eead8
Máximo Huamán Allccahuamán <72884594+maximohuamanallccahuaman@users.noreply.github.com>
2023-02-27 21:48:42 -0500

    Delete src directory

--- src/main/java/com/reto/Reto01/repository/CondicionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reto.Reto01.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.reto.Reto01.model.Condicion;
-
-public interface CondicionRepository extends JpaRepository<Condicion, Integer> {
-
-}
